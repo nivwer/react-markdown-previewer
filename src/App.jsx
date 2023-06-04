068c322317dad25db8f3c9943a7233169b9b34ff
nivwer <nivwerr@gmail.com>
2023-06-04 18:30:05 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Markdown from "./components/markdown/Markdown";
+
+function App() {
+  return (
+    <>
+      <Markdown />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/markdown/Markdown.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { marked } from "marked";
+
+
+function Markdown() {
+  
+  marked.setOptions({
+    mangle: false,
+    headerIds: false,
+    breaks: true,
+    gfm: true,
+  });
+
+  function useMarked() {
+    const [text, setText] = useState("# h1 Example");
+    const event = (e) => {
+      setText(e.target.value);
+    };
+    const markdown = marked(text);
+
+    return [text, { event, markdown }];
+  }
+
+  const [text, { event, markdown }] = useMarked();
+
+  return (
+    <>
+      <div className="bg-black row min-vh-100 vw-100 mx-auto">
+        <div className="col-lg-6 p-4 container_responsive">
+          <h2 className="text-center text-light-emphasis">Editor</h2>
+            <textarea
+              id="editor"
+              className="text-dark-emphasis text-break w-100 border border-end-0 border-dark border-1 p-4 overflow-auto bg-black"
+              onChange={event}
+              value={text}
+            ></textarea>
+        </div>
+        <div className="col-lg-6 p-4 container_responsive">
+          <h2 className="text-center text-light-emphasis">Preview</h2>
+          <div
+            id="preview"
+            className="text-dark-emphasis text-break w-100 p-4 overflow-auto border border-end-0 border-dark border-1"
+            dangerouslySetInnerHTML={{ __html: markdown }}
+          ></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Markdown;
