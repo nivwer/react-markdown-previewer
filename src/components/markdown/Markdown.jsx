4ae61338b37dec74ff04d34ddee8856df4068134
nivwer <nivwerr@gmail.com>
2023-06-04 20:40:39 -0300

    add useEffect (data.txt)

--- src/components/markdown/Markdown.jsx
+++ src/components/markdown/Markdown.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { marked } from "marked";
 
-
 function Markdown() {
-  
   marked.setOptions({
     mangle: false,
     headerIds: false,
@@ -12,10 +10,25 @@ function Markdown() {
   });
 
   function useMarked() {
-    const [text, setText] = useState("# h1 Example");
+    const [text, setText] = useState(" ");
     const event = (e) => {
       setText(e.target.value);
     };
+
+    useEffect(() => {
+      const fetchText = async () => {
+        try {
+          const response = await fetch('data.txt');
+          const content = await response.text();
+          setText(content);
+        } catch (error) {
+          console.error("Error fetching Data:", error);
+        }
+      };
+
+      fetchText();
+    }, []);
+
     const markdown = marked(text);
 
     return [text, { event, markdown }];
@@ -28,12 +41,12 @@ function Markdown() {
       <div className="bg-black row min-vh-100 vw-100 mx-auto">
         <div className="col-lg-6 p-4 container_responsive">
           <h2 className="text-center text-light-emphasis">Editor</h2>
-            <textarea
-              id="editor"
-              className="text-dark-emphasis text-break w-100 border border-end-0 border-dark border-1 p-4 overflow-auto bg-black"
-              onChange={event}
-              value={text}
-            ></textarea>
+          <textarea
+            id="editor"
+            className="text-dark-emphasis text-break w-100 border border-end-0 border-dark border-1 p-4 overflow-auto bg-black"
+            onChange={event}
+            value={text}
+          ></textarea>
         </div>
         <div className="col-lg-6 p-4 container_responsive">
           <h2 className="text-center text-light-emphasis">Preview</h2>
